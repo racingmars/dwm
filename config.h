bf45704e299bdb8d772f5e4430be4aa6d275f097
Matthew R. Wilson <mwilson@mattwilson.org>
2020-06-09 01:42:18 -0700

    add more terminals

--- config.h
+++ config.h
@@ -38,6 +38,8 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "st",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ "Alacritty", NULL,   NULL,           0,         0,          1,          -1,        -1 },
+	{ "urxvt",   NULL,     NULL,           0,         0,          1,          -1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 };
 
