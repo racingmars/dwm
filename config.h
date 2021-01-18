efcb60735e836f4f8ebc092f43b761d047dd8731
Matthew R. Wilson <mwilson@mattwilson.org>
2021-01-18 03:51:42 -0800

    Switch to xterm

--- config.h
+++ config.h
@@ -40,6 +40,7 @@ static const Rule rules[] = {
 	{ "st",      NULL,     NULL,           0,         0,          1,          -1,        -1 },
 	{ "Alacritty", NULL,   NULL,           0,         0,          1,          -1,        -1 },
 	{ "urxvt",   NULL,     NULL,           0,         0,          1,          -1,        -1 },
+	{ "XTerm",   NULL,     NULL,           0,         0,          1,           1,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         1,          0,           1,        -1 }, /* xev */
 	{ "Remote-viewer", NULL, NULL,         0,         1,          0,           1,        -1 }, /* remote-viewer */
 	{ "pcsws.exe", NULL,   NULL,           0,         1,          0,           1,        -1 }, /* PCOMM in wine */
@@ -74,7 +75,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[]  = { "xterm", NULL };
 static const char *webcmd[]   = { "firefox", NULL };
 static const char *altwebcmd[]= { "chromium", NULL };
 
